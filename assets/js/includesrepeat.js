27a2000722612c8194cb33399e391e37841f795a
Felipe Guimarães <fguiengenharia@gmail.com>
2021-01-17 00:03:57 -0300

    Includes & Repeat

---
+++ assets/js/includesrepeat.js
@@ -0,0 +1,19 @@
+/*
+    Usando a função includes podemos fazer uma verificação
+    de retorno true ou false em um array por exemplo, ou em uma string.
+
+*/
+/*
+    Array
+    let nome = [ 'Beltrano', 'Cicrano', 'Fulano' ];
+    console.log(nome.includes('w'));
+*/
+
+/*
+String
+let teste = 'QWERTYUIOPASDFGHJKLÇZXCVBN';
+console.log(teste.includes('M'));
+*/
+
+//Usando a função repeat
+console.log('10k'.repeat(10));
